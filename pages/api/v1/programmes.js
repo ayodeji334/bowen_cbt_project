73734d2ec33216a5a8cbcf4f9848406dc738600e
ayodeji334 <fawumiayodeji@gmail.com>
2021-04-16 11:32:19 +0100

    connection to db modified

--- pages/api/v1/programmes.js
+++ pages/api/v1/programmes.js
@@ -1,10 +1,15 @@
 import mysql from 'mysql'
 
+const host = process.env.NODE_ENV === 'production' ? process.env.PROD_DATABASE_HOST : process.env.DATABASE_HOST ,
+user = process.env.NODE_ENV === 'production' ? process.env.PROD_DATABASE_USER : process.env.DATABASE_USER,
+password = process.env.NODE_ENV === 'production' ? process.env.PROD_DATABASE_PASSWORD : process.env.DATABASE_PASSWORD,
+database = process.env.NODE_ENV === 'production' ? process.env.PROD_DATABASE_NAME : process.env.DATABASE_NAME;
+
 const dbConnect = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER ,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
+    host,
+    user,
+    password,
+    database
 });
 
 dbConnect.connect(err => {
