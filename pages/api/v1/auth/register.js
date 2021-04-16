05c5741297e743dc757f16bc4e42b9fbb7895c23
ayodeji334 <fawumiayodeji@gmail.com>
2021-04-16 11:18:51 +0100

    node env db connection setup

--- pages/api/v1/auth/register.js
+++ pages/api/v1/auth/register.js
@@ -2,11 +2,16 @@ import mysql from 'mysql'
 import jwt from 'jsonwebtoken'
 import cookie from 'cookie'
 
+const host = process.env.NODE_ENV ==='production' ? process.env.PROD_DATABASE_HOST : process.env.DATABASE_HOST ,
+user = process.env.NODE_ENV ==='production' ? process.env.PROD_DATABASE_USER : process.env.DATABASE_USER,
+password = process.env.NODE_ENV ==='production' ? process.env.PROD_DATABASE_PASSWORD : process.env.DATABASE_PASSWORD,
+database = process.env.NODE_ENV ==='production' ? process.env.PROD_DATABASE_NAME : process.env.DATABASE_NAME;
+
 const dbConnect = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER ,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
+    host,
+    user,
+    password,
+    database
 });
 
 dbConnect.connect(err => {
